e9458e322d5576917bcf7553635effad7e09b6eb
McUberStein <95429589+McUberStein@users.noreply.github.com>
2022-04-14 18:24:47 +0200

    Update main.py

--- judgment/main.py
+++ judgment/main.py
@@ -12,15 +12,15 @@ elif any(srchstr in waifu for srchstr in ('komi' , 'speedwagon' , 'dio' , 'ishig
  print("You are a god")
 elif any(srchstr in waifu for srchstr in ('chomosuke' , 'komeko' , 'melty')):
  print("Bruh, what?")
- playsound.playsound('snd1.mp3')
+ playsound.playsound('snd/snd1.mp3')
 elif any(srchstr in waifu for srchstr in ('asuna' , 'sylvia' , 'mikasa')):
  print("Meh, alright")
 elif any(srchstr in waifu for srchstr in ('bismarck' , 'eugen' , 'selphius' , 'friedrich' , 'elbe')):
  print("Deutschland indeed")
- playsound.playsound('snd2.mp3')
+ playsound.playsound('snd/snd2.mp3')
 elif any(srchstr in waifu for srchstr in ('giorno')):
  print("GOLDEN WIND")
- playsound.playsound('snd3.mp3')
+ playsound.playsound('snd/snd3.mp3')
 #elif not {'wiz' , 'zerotwo' , 'astolfo' , 'enterprise' , 'megumin' ,'gabi' , 'ichigo' , 'malty' ,'toga' , 'marin' , 'darkness' , 'aqua' , 'kaguya' , 'chika' , 'midari' , 'holo' , 'komi' , 'speedwagon' , 'dio' , 'ishigami' , 'chomosuke' , 'komeko' , 'chica' , 'freddy' , 'bonnie' , 'vanessa' , 'melty' , 'giorno' , 'asuna' , 'sylvia' , 'mikasa' ,'bismarck' , 'eugen' , 'selphius' , 'friedrich' , 'elbe' }.isdisjoint(waifu.split()):
 else:
  print("Sorry, that isn't a valid choice. Please yell at McUberStein to implement it later.")
